89be965212317fe9424fd6bd5529107c3ad3f79f
shishelevevgen <shisheleveo@yandex.ru>
2023-02-07 08:50:28 +0300

    Multiple cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,19 @@
 # encoding: utf-8
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+cards = []
 
-puts "#{values.sample} of #{suits.sample}"
+values.each do |value|
+  suits.each do |suit|
+    cards << "#{value} of #{suit}"
+  end
+end
+
+cards.shuffle!
+
+puts 'How many cards do you pick?'
+number = STDIN.gets.to_i
+
+number.times do 
+  puts cards.pop
+end
